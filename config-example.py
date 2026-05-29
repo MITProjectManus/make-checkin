7eaf90fe80d4c11a8d6aa92ed6f956a8545aa6df
Oliver Thomas <othomas@othom.as>
2026-05-29 07:42:28 -0400

    Initial commit

---
+++ config-example.py
@@ -0,0 +1,7 @@
+#
+# Example location configuration file. The production file
+# needs to be named "config.py" and managed locally for 
+# each kiosk deployment. It will contain API keys and is
+# not tracked in the repository. You can use this example
+# file for reference in creating your local "config.py".
+
